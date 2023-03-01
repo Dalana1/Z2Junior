53c0c49153d65cf3836d992aa7b42310602e7d52
Kamila Dawid <dalana.kam22@gmail.com>
2023-03-01 10:33:32 +0100

    calculator ver 2.0

--- f-102-exam/myscript.js
+++ f-102-exam/myscript.js
@@ -1,12 +1,11 @@
-const First_num = +prompt("Podaj pierwszą liczbę",'');
-// First_num = new Number();
+let First_num = +prompt("Podaj pierwszą liczbę",'');
 
 
-const Math_action= prompt("Podaj operator arytmetyczny (+, -, *, / lub %");
+let Math_action= prompt("Podaj operator arytmetyczny (+, -, *, / lub %");
 
 
 
-const Second_num = +prompt("Podaj drugą liczbę", '');
+let Second_num = +prompt("Podaj drugą liczbę", '');
 
 
 // if (First_num === "" && Second_num === "" && Math_action === ""){
@@ -20,45 +19,69 @@ const Second_num = +prompt("Podaj drugą liczbę", '');
 // } 
 
 
-function calcSum (First_num, Second_num) {
-    if (Math_action == '+'){ 
-    return First_num+Second_num
+function calcSum(First_num, Second_num) {
+    return First_num + Second_num;
+  }
+  
+  function calcDiff(First_num, Second_num) {
+    return First_num - Second_num;
+  }
+  
+  function calcQuotient(First_num, Second_num) {
+    return First_num / Second_num;
+  }
+  
+  function calcProduct(First_num, Second_num) {
+    return First_num * Second_num;
+  }
+  
+
+
+// let sum = calcSum(First_num, Second_num);
+// alert (sum);
+
+// let diff = calcDiff (First_num, Second_num);
+// alert (diff);
+
+// let quotient = calcQuotient(First_num, Second_num);
+// alert (quotient);
+
+// let product = calcProduct(First_num, Second_num);
+// alert (product);
+
+
+let result;
+
+do {
+    switch (Math_action) {
+
+        case "+":
+            result = calcSum(First_num, Second_num);
+        break;
+
+        case "-":
+            result = calcDiff(First_num, Second_num);
+        break;
+
+        case "/":
+            result = calcQuotient(First_num, Second_num);
+        break;
+
+        case "*":
+            result = calcProduct(First_num, Second_num);
+        break;
+
+        default:
+            alert("wybierz z podanych akcji: +, -, *, / lub %");
+        break;
     }
-}
 
-function calcDiff (First_num, Second_num) {
-    if (Math_action == '-'){ 
-    return First_num-Second_num
-    }
-}
+    alert(result);
 
-function calcQuotient (First_num, Second_num) {
-    if (Math_action == '/'){ 
-    return First_num/Second_num
-    }
-}
+    Math_action = prompt("Podaj operator arytmetyczny (+, -, *, / lub %) lub zostaw puste żeby zakończyć");
+    First_num = result;
+    Second_num = +prompt("Podaj kolejną liczbę", "");
 
-function calcProduct (First_num, Second_num) {
-    if (Math_action == '*'){ 
-    return First_num*Second_num
-    }
-}
+} while (Math_action !== null);
 
-
-let sum = calcSum(First_num, Second_num);
-alert (sum);
-
-let diff = calcDiff (First_num, Second_num);
-alert (diff);
-
-let quotient = calcQuotient(First_num, Second_num);
-alert (quotient);
-
-let product = calcProduct(First_num, Second_num);
-alert (product);
-
-
-do 
-{
-    // dopóki pole edycyjne true
-}
+alert("Koniec programu");
